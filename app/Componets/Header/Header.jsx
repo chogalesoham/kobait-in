64e0b545f02734020ef96189f599dc2e1ee0e88c
soham chogale <chogalesoham74@gmail.com>
2024-11-02 17:00:21 +0530

    style/change nav-bar style

--- app/Componets/Header/Header.jsx
+++ app/Componets/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
   };
 
   return (
-    <nav className="fixed w-full z-10 bg-[#FEF9F2] bg-gradient-to-r from-[#F9CEB7] to-[#ADD1F2] shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] shadow-lg">
+    <nav className="fixed w-full z-10 bg-slate-50 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
@@ -23,7 +23,7 @@ function Header() {
         </a>
         <button
           onClick={toggleMenu}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-pink-600 rounded-lg md:hidden hover:bg-pink-100 focus:outline-none focus:ring-2 focus:ring-pink-200"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center rounded-lg md:hidden hover:bg-pink-100 focus:outline-none focus:ring-2 focus:ring-pink-200"
           aria-controls="navbar-solid-bg"
           aria-expanded={isMenuOpen}
         >
@@ -54,7 +54,7 @@ function Header() {
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-pink-600 my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0"
+                className="block py-2 px-3 text-[#000]  my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-[#ff0000] md:p-0"
               >
                 Home
               </Link>
@@ -62,7 +62,7 @@ function Header() {
             <li>
               <Link
                 href="/about"
-                className="block py-2 px-3 text-pink-600 my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0"
+                className="block py-2 px-3 text-[#000] my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-[#ff0000] md:p-0"
               >
                 About Us
               </Link>
@@ -70,7 +70,7 @@ function Header() {
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-pink-600 my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0"
+                className="block py-2 px-3 text-[#000] my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-[#ff0000] md:p-0"
               >
                 Services
               </Link>
@@ -78,7 +78,7 @@ function Header() {
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-pink-600 my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0"
+                className="block py-2 px-3 text-[#000] my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-[#ff0000] md:p-0"
               >
                 Pricing
               </Link>
@@ -86,7 +86,7 @@ function Header() {
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-pink-600 my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-700 md:p-0"
+                className="block py-2 px-3 text-[#000] my-1 text-xl rounded hover:bg-pink-100 md:hover:bg-transparent md:border-0 md:hover:text-[#ff0000] md:p-0"
               >
                 Contact Us
               </Link>
