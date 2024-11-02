ee708ab1c1340babd024c8dcfedae74bf67b1688
soham chogale <chogalesoham74@gmail.com>
2024-11-02 12:09:16 +0530

    feat/cerate about-us hero-section

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../Componets/Header/Header";
 import Footer from "../Componets/Footer/Footer";
+import AboutHeroSection from "../Componets/AboutHeroSection/AboutHeroSection";
+import GetInTouch from "../Componets/GetInTouch/GetInTouch";
 
 const AboutUS = () => {
   return (
     <>
       <Header />
+      <AboutHeroSection />
+      <GetInTouch />
       <Footer />
     </>
   );
